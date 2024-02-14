ccbd44a9337549e9a3736c2d4626b8e6f310e1b4
Haroldfromk <97341336+Haroldfromk@users.noreply.github.com>
2024-02-15 06:33:53 +0900

    [level 0] Title: 공 던지기, Time: 0.01 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.swift
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.swift
@@ -1,10 +1,5 @@
 import Foundation
 
 func solution(_ numbers:[Int], _ k:Int) -> Int {
-    
-    var answer : Int = 0
-    
-    answer = numbers[((k-1)*2)%numbers.count]
-
-    return answer
+    return 0
 }
