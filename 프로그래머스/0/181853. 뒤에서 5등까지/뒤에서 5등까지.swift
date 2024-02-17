90c86f0e512416bc01a91ccec917bf331684bf2f
Haroldfromk <97341336+Haroldfromk@users.noreply.github.com>
2024-02-17 10:48:51 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.12 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ num_list:[Int]) -> [Int] {
+    
+    var answer : [Int] = []
+    
+    answer = Array(num_list.sorted()[0...4])
+    
+    
+    return answer
+}
