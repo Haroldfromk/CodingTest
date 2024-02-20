8cb4e7959b062d1bb68973c3eb162cf7f80b76e8
Haroldfromk <97341336+Haroldfromk@users.noreply.github.com>
2024-02-20 12:35:06 +0900

    [level 1] Title: 직사각형 별찍기, Time: 6.00 ms, Memory: 14.2 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.swift
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.swift
@@ -3,16 +3,11 @@ import Foundation
 let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
 let (a, b) = (n[0], n[1])
 
-var star : String = "*"
-var hor : Int = 1
-var ver : Int = 0
+var row = Array(repeating: "*",count:a).joined()
 
-while hor != a{
-    star += "*"
-    hor+=1
+for i in 0..<b {
+    print(row)
 }
-   
-while ver != b {
-    print(star)
-    ver+=1
-}
+
+
+
