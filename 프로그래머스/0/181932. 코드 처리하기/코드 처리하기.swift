a2fec075c2272b20b7a4fffb400c8aa2d005126a
Haroldfromk <97341336+Haroldfromk@users.noreply.github.com>
2024-02-22 14:03:31 +0900

    [level 0] Title: 코드 처리하기, Time: 0.13 ms, Memory: 16 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+func solution(_ code:String) -> String {
+    
+    var mode : Int = 0
+    var ret : String = ""
+    var count : Int = 0
+    var arr = code.map{String($0)}.enumerated().map{$0}
+    
+    var i : Int = 0
+
+    
+    count = arr.count-1
+    
+    while i <= count {
+        if mode == 0 {
+            if arr[i].element != "1" && arr[i].offset%2 == 0{
+                ret += arr[i].element
+            } else if arr[i].element == "1" {
+                mode = 1
+            } 
+        } else {
+            if arr[i].element != "1" && arr[i].offset%2 != 0{
+                ret += arr[i].element
+            } else if arr[i].element == "1" {
+                mode = 0
+            }
+        }
+        i+=1
+    }
+    
+    if ret == "" {
+        ret = "EMPTY"
+    }
+   
+    return ret
+}
