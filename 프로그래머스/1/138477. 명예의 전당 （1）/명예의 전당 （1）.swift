c8bb3974d392a7ff05595d4f9af51f09e1c3f098
Haroldfromk <97341336+Haroldfromk@users.noreply.github.com>
2024-03-07 07:02:48 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.05 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.swift
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.swift
@@ -1,5 +1,26 @@
 import Foundation
 
 func solution(_ k:Int, _ score:[Int]) -> [Int] {
-    return []
+    
+    var result : [Int] = []
+    var halloffame : [Int] = []
+    
+    for i in score.indices {
+        if i < k {
+            halloffame.append(score[i])
+            result.append(halloffame.min()!)
+        } else {
+            if score[i] >= halloffame.min()! {
+                halloffame.append(score[i])
+                halloffame.remove(at:halloffame.firstIndex(of:halloffame.min()!)!)
+                result.append(halloffame.min()!)
+                
+            } else {
+                result.append(halloffame.min()!)
+            }
+        }
+        
+    }
+
+    return result
 }
