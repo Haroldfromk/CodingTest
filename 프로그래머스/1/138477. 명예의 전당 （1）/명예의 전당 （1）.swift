fe350e21ac0462e9510354b8733c42611037f2ef
Haroldfromk <97341336+Haroldfromk@users.noreply.github.com>
2024-03-07 11:01:01 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.swift
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.swift
@@ -2,21 +2,21 @@ import Foundation
 
 func solution(_ k:Int, _ score:[Int]) -> [Int] {
     
-    var result : [Int] = []
+    var result : [Int] = [] // 발표점수
     var halloffame : [Int] = []
-    
-    for i in score.indices {
-        if i < k {
-            halloffame.append(score[i])
-            result.append(halloffame.min()!)
-        } else {
-            if score[i] >= halloffame.min()! {
-                halloffame.append(score[i])
-                halloffame.remove(at:halloffame.firstIndex(of:halloffame.min()!)!)
-                result.append(halloffame.min()!)
+
+    for i in score.indices { 
+        if i < k { // 초기 k일까지.
+            halloffame.append(score[i]) // 명예의 전당에 해당일차의 score값을 추가.
+            result.append(halloffame.min()!) // 발표점수에 최하위 점수를 추가.
+        } else { // k일 이후
+            if score[i] >= halloffame.min()! { // score값이 명전 최소값과 크거나 같다면?
+                halloffame.append(score[i]) // 명예의 전당에 해당 score값을 추가
+                halloffame.remove(at:halloffame.lastIndex(of:halloffame.min()!)!) // 최소값에 해당하는 위치의 값을 명예의 전당에서 삭제
+                result.append(halloffame.min()!) // 발표점수에 최하위 점수를 추가
                 
-            } else {
-                result.append(halloffame.min()!)
+            } else { // score값이 작다면?
+                result.append(halloffame.min()!) // 발표점수에 최하위 점수를 추가
             }
         }
         
